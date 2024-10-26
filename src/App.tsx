cafc0bb3ea5eed6ab84dc4a6b4240af3f6e2996b
Julia Fokanova <oncologmma@mail.ru>
2024-10-26 20:21:01 +0300

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,77 @@
+import { useEffect, useState } from 'react';
+import { pingAPI, getAPI, postAPI, deleteAPI } from './libs/api';
 import Form from './components/Form/Form';
+import Load from './components/Load/Load';
 import NoteList from './components/NoteList/NoteList';
+import ServerError from './components/ServerError/ServerError';
 import Title from './components/Title/Title';
 
 const App = () => {
-  return (
+  const [note, setNote] = useState(''); // текст заметки -> "Lorem"
+  const [list, setList] = useState([]); // массив заметок [{id: 0, content: "Lorem"}, {...}, ...]
+  const [isLoad, setIsLoad] = useState(false); // подключение к серверу
+  const [isFound, setIsFound] = useState(false); // ошибка подключения к серверу
+
+  // первоначальная отрисовка компонента с данными, полученными с сервера:
+  useEffect(() => {
+    (async () => {
+      const server = await pingAPI(); // ждём ответа от сервера
+      setIsLoad(true);
+
+      // обработка ошибки подключения к серверу:
+      if (server.status === 520) {
+        return; // выходим и отрисовываем ошибку сервера
+      }
+
+      setIsFound(true); // связь с сервером установлена
+      const data = await getAPI(); // GET-запрос на сервер
+      setList(data); // ререндер (состояние list) -> актуализация массива заметок
+    })();
+  }, []);
+
+  // обработчик cобытия 'change' на textarea:
+  const handleChange = (textNote: string) => {
+    setNote(textNote); // ререндер (состояние note) -> обновление поля textarea
+  };
+
+  // обработчик cобытия 'submit' на форме:
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>,
+    textNote: string
+  ) => {
+    event.preventDefault();
+    if (!textNote) {
+      setNote(''); // ререндер (состояние note) -> сброс невалидного textarea
+      return;
+    }
+
+    await postAPI(note); // POST-запрос на сервер
+    const data = await getAPI(); // GET-запрос на сервер
+    setList(data); // ререндер (состояние list) -> актуализация массива заметок
+    setNote(''); // ререндер (состояние note) -> очистка полей формы
+  };
+
+  const onRefresh = async () => {
+    const data = await getAPI(); // GET-запрос на сервер
+    setList(data); // ререндер (состояние list) -> актуализация массива заметок
+    setNote(''); // ререндер (состояние note) -> очистка полей формы
+  };
+
+  const onRemove = async (id: number) => {
+    await deleteAPI(id); // DELETE-запрос на сервер
+    const data = await getAPI(); // GET-запрос на сервер
+    setList(data); // ререндер (состояние list) -> актуализация массива заметок
+  };
+
+  return !isLoad ? (
+    <Load /> // FIXME: как убрать мелькание компонента Load при загрузке и перезагрузке веб-страницы
+  ) : !isFound ? (
+    <ServerError />
+  ) : (
     <>
-      <Title />
-      <NoteList />
-      <Form />
+      <Title onRefresh={onRefresh} />
+      <NoteList list={list} onRemove={onRemove} />
+      <Form note={note} onChange={handleChange} onSubmit={handleSubmit} />
     </>
   );
 };
--- src/components/NoteItem/NoteItem.tsx
+++ src/components/NoteItem/NoteItem.tsx
@@ -1,15 +1,16 @@
 import './noteItem.css';
 
-const NoteItem = () => {
+interface INoteItemProps {
+  id: number;
+  content: string;
+  onRemove: (id: number) => void;
+}
+
+const NoteItem = ({ id, content, onRemove }: INoteItemProps) => {
   return (
     <li className="note-item">
-      <p className="note-item__content">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil eius,
-        exercitationem consequatur voluptas nobis commodi quibusdam, culpa
-        necessitatibus debitis veniam numquam voluptatum dignissimos, quisquam
-        inventore excepturi? Rerum eos vel quis.
-      </p>
-      <div className="note-item__close-btn">
+      <p className="note-item__content">{content}</p>
+      <div className="note-item__close-btn" onClick={() => onRemove(id)}>
         <span className="note-item__close-btn_first"></span>
         <span className="note-item__close-btn_second"></span>
       </div>
